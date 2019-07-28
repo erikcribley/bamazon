fb403027a25185c5465f02d0158d22c8ef5d03f3
Erik Cribley <erikcribley@gmail.com>
2019-07-28 03:48:33 -0400

    view all and view low inventory functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,6 +11,42 @@ let connection = mysql.createConnection({
     database: 'bamazon'
 })
 
+function viewProducts () {
+    let query = "SELECT * FROM products"
+    connection.query(query, function(err, res) {
+        if (err) {
+            throw err
+        }
+        for (i = 0; i < res.length; i++) {
+            console.log(
+                res[i].item_id + " | " + 
+                res[i].product_name + " | " +
+                res[i].department_name + " | " +
+                res[i].price + " | " +
+                res[i].stock_quantity
+            )
+        }
+    })
+}
+
+function viewLow () {
+    let query = "SELECT * FROM products WHERE stock_quantity < 5"
+    connection.query(query, function(err, res) {
+        if (err) {
+            throw err
+        }
+        for (i = 0; i < res.length; i++) {
+            console.log(
+                res[i].item_id + " | " + 
+                res[i].product_name + " | " +
+                res[i].department_name + " | " +
+                res[i].price + " | " +
+                res[i].stock_quantity
+            )
+        } 
+    })
+}
+
 //run
 connection.connect(function(err) {
     if (err) {
@@ -18,3 +54,5 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId)
 })
+
+viewLow()
