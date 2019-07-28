872f98d1de47f7bd2fe20d1ba36e8d3b5e1f1432
Erik Cribley <erikcribley@gmail.com>
2019-07-28 02:23:32 -0400

    functions coded

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,21 +11,51 @@ let connection = mysql.createConnection({
     database: 'bamazon'
 })
 
+//reduce stock
+function reduceStock (id, units, r) {
+    connection.query("UPDATE products SET stock_quantity =? WHERE item_id=?", 
+                    [r.stock_quantity - units, id], 
+                    function(err, res) {
+        if (err) {
+            throw err
+        }
+        let price = r.price * units
+        console.log("Total Price: " + price)
+    })
+}
+
+//removing purchased product from database
+function checkStock (id, units) { 
+    connection.query("SELECT * FROM products WHERE item_id =?", id , function(err, res) {
+        if (err) {
+            throw err
+        }
+        if (units <= res[0].stock_quantity) {
+           reduceStock(id, units, res[0])
+        } else {
+            console.log("Insufficient Quantity!")
+        }
+    })
+}
+
 //inquirer
-function makePurchase (){
+function makePurchase () {
     inquirer
         .prompt([
             {
                 type: "input",
                 message: "Please enter the ID number of the item you'd like to purchase",
-                name: "item-id"
+                name: "itemID"
             },
             {
                 type: "input",
                 message: "How many units would you like to purchase?",
-                name: "units-sold"
+                name: "unitsSold"
             }
         ])
+        .then(function(answer) {
+            checkStock(answer.itemID, answer.unitsSold)
+        })
 }
 
 //display all items
@@ -34,14 +64,14 @@ function displayAll () {
         if (err) {
             throw err
         }
-        console.log(res)
-        // for (i = 0; i = res.length; i++) {
-        //     console.log(res[i].item_id + " | " + 
-        //                 res[i].product_name + " | " +
-        //                 res[i].department_name + " | " +
-        //                 res[i].price + " | " +
-        //                 res[i].stock_quantity)
-        // }
+        // console.log(res)
+        for (i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + 
+                        res[i].product_name + " | " +
+                        res[i].department_name + " | " +
+                        res[i].price + " | " +
+                        res[i].stock_quantity)
+        }
         makePurchase()
     })
 }
